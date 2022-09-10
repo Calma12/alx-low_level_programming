8d9686ba69e39841995362e7942d00e17d658887
Calma12 <Calebedeh2004@gmail.com>
2022-09-09 17:11:50 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
-nclude <stdio.h>
+include <stdio.h>
 
-//Compiler version gcc  6.3.0
-//
-//int main()
+/**
+ * main - print the string in the put function
+ *
+ * Description: Using the main function
+ * this program prints "Programming is like building a multilingual puzzle
+ * return:0
+ */
+//int main(void)
 //{
 //  puts("Programming is like building a multilingual puzzle");
 //    return 0;
