d7b20ff03363e824ed1450a01fd068ced0bc7862
Calma12 <Calebedeh2004@gmail.com>
2022-09-12 16:51:40 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 /**
-* main - print tge string in the put function
-*
-* Description: using the main function
-* this program prints "Programming is like building a multilingual puzzle
-* Return: 0
-*/
-Int main(void)
+ * main - print tge string in the put function
+ *
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return: 0
+ */
+int main(void)
 {
-       puts("/"Programming is like building a multilingual puzzle");
-       Return: (0)
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0):
 }
+
